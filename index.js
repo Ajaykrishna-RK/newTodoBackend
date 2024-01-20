2c638288f78b1e96bcb97d36652312dc369184f1
Ajay <ajayajuu73@gmail.com>
2024-01-20 17:22:17 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!"); 
+  res.send("Hello, World!");
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000");
 });
