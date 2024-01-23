cc44e3fefa9abaae171828f03c593383495804fb
Ajay <ajayajuu73@gmail.com>
2024-01-23 19:47:57 +0530

    cors added

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 
 const dotenv = require("dotenv");
-const todoRouter = require("./routes/TodoRouter")
-const dbCOnnection = require("./config/dbConnection")
+const cors = require("cors");
+const todoRouter = require("./routes/TodoRouter");
+const dbCOnnection = require("./config/dbConnection");
 
 dotenv.config();
 const app = express();
-dbCOnnection()
-app.use(express.json())
+dbCOnnection();
+app.use(cors());
+app.use(express.json());
 
-app.use("/",todoRouter );
+app.use("/", todoRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000");
